e66ffc368ac453b143103f8e4eb7a93d85ea9094
TNK145 <thanakornimboon@gmail.com>
2025-12-03 23:11:19 +0700

    add files

---
+++ Streamlit-app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+#streamlit run "c:/Users/hp/Desktop/University Homeworks/Basic Prog/[LAST PROJECT]/Streamlit-app.py"
+
+# The title of your web application
+st.title('My First Streamlit App')
+
+# A header for a section
+st.header('Hello, Streamlit World!')
+
+# The 'magic' write command can handle almost anything
+st.write('This web app is running entirely from Python.')
+
+# Display text formatted with Markdown
+st.markdown('### Streamlit makes development **easy** and **fast**.')
